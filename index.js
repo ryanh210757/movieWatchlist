42ea4e9db454ab4a3f57130e6b7bb120e10cdcd8
ryanh210757 <kryanharris1@gmail.com>
2023-01-17 15:20:11 -0600

    start adding data

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let movieTitle
+let title
 
 document.getElementById("searchBtn").addEventListener("click", function (){
     movieTitle = document.getElementById("movieTitle").value
@@ -7,16 +8,33 @@ document.getElementById("searchBtn").addEventListener("click", function (){
     .then(data => {
         console.log(movieTitle +"s")
         console.log(data)
+
         let html = ""
         for(let item of data.Search){
-            html += 
-            `
-            <img src=${item.Poster} />
-            <h1>${item.Title}</h1>
-            <h3>${item.Year}</h3> 
-            `
+            fetch(`http://www.omdbapi.com/?i=${item.imdbID}&plot=short&apikey=c9792923`)
+                .then(res=> res.json())
+                .then(data => {
+                    console.log(data)
+                    html += 
+                    `
+                    <div class="movieLayout">
+                        <img src="${data.Poster}"/>
+                        <div class="movieContainer">
+                        <h2>${data.Title}</h2>
+                        <p>${data.imdbRating}</p>
+                        <div class="movieStats">
+                            <p>${data.Runtime}</p>
+                            <p>${data.Genre}</p>
+                            <button>Watchlist</button
+                        </div>
+                        <p>${data.Plot}</p>
+                    </div>
+                    </div>
+                    `
+                    document.getElementById("movieList").innerHTML =  html        
+                })
+            
         }
-        document.getElementById("movieList").innerHTML =  html
         console.log(html)
     })    
 })
