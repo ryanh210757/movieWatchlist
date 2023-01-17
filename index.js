591731f48f998ed446bfa02bd47aa6cea14bcdc9
ryanh210757 <kryanharris1@gmail.com>
2023-01-16 21:37:30 -0600

    fetch movie from input

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
-document.getElementById("searchBtn").addEventListener("click", function (){
-    fetch("http://www.omdbapi.com/?t=blade&apikey=c9792923")
-    .then(res => res.json())
-    .then(data => console.log(data))    
-})
+let movieTitle
 
+document.getElementById("searchBtn").addEventListener("click", function (){
+    movieTitle = document.getElementById("movieTitle").value
+    fetch(`http://www.omdbapi.com/?s=${movieTitle}&apikey=c9792923`)
+    .then(res => res.json())
+    .then(data => {
+        console.log(movieTitle +"s")
+        console.log(data)
+        let html = ""
+        for(let item of data.Search){
+            html += 
+            `
+            <img src=${item.Poster} />
+            <h1>${item.Title}</h1>
+            <h3>${item.Year}</h3> 
+            `
+        }
+        document.getElementById("movieList").innerHTML =  html
+        console.log(html)
+    })    
+})
